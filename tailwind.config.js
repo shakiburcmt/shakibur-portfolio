1528cf5481d96e2a809b318efce0860e1da2bf13
Md. Shakibur Rahman <shakibur.cmt@gmail.com>
2024-09-11 08:47:37 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: "15px",
+		},
+		screens: {
+		},
+		extend: {
+			keyframes: {
+				"accordion-down": {
+					"from": { height: "0" },
+					"to": { height: "var(--radix-accordion-content-height)" },
+				},
+			},
+		},
+	},
+	
+  theme: {
+  	extend: {
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+};
